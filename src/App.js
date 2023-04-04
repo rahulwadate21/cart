652539b41aa9d9fe91774fb6a09bc5a044f7b0ee
rahulwadate21 <115419561+rahulwadate21@users.noreply.github.com>
2023-04-04 17:40:24 +0530

    add cart component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import CartItem from './CartItem';
+import Cart from './Cart';
 
 function App() {
   return (
     <div className="App">
-     <CartItem />
+     <Cart />
     </div>
   );
 }
