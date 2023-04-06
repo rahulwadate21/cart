64d9b1f989db239af6d0a8f40bd951ab0f524533
rahulwadate21 <115419561+rahulwadate21@users.noreply.github.com>
2023-04-06 15:38:11 +0530

    Add total price

--- src/App.js
+++ src/App.js
@@ -9,24 +9,24 @@ class App extends React.Component {
     this.state = {
       products: [
         {
-          price: 99,
+          price: 1999,
           title: 'Watch',
           qty: 1,
-          img: '',
+          img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80',
           id: 1
         },
         {
-          price: 999,
-          title: 'Mobile Phone',
+          price: 4999,
+          title: ' Tele-Phone',
           qty: 10,
-          img: '',
+          img: 'https://images.unsplash.com/photo-1520923642038-b4259acecbd7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=819&q=80',
           id: 2
         },
         {
-          price: 999,
+          price: 49999,
           title: 'Laptop',
           qty: 4,
-          img: 'https://images.unsplash.com/photo-1504707748692-419802cf939d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1330&q=80',
+          img: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80',
           id: 3
         }
       ]
@@ -82,6 +82,18 @@ class App extends React.Component {
     return count;
   }
 
+  getCartTotal = () => {
+    const { products } = this.state;
+
+    let cartTotal = 0;
+
+    products.map((product) => {
+      cartTotal = cartTotal + product.qty * product.price
+    })
+
+    return cartTotal;
+  }
+
   render () {
     const { products } = this.state;
     return (
@@ -93,6 +105,7 @@ class App extends React.Component {
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteProduct={this.handleDeleteProduct}
         />
+       <div style={ {padding: 10, fontSize: 20} }>TOTAL: {this.getCartTotal()} </div>
       </div>
     );
   }
--- src/Cart.js
+++ src/Cart.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CartItem from './CartItem';
+import './App.css';
 
 const Cart = (props) => {
   const { products } = props;
